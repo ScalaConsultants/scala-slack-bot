36d390fca2740c0fc49581afa11ed40b100b18f5
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-02-12 21:33:32 +0100

    basic parse and reply

--- src/main/scala/io/scalac/slack/bots/digest/DigestBot.scala
+++ src/main/scala/io/scalac/slack/bots/digest/DigestBot.scala
@@ -2,7 +2,7 @@ package io.scalac.slack.bots.digest
 
 import akka.actor.{Actor, ActorLogging, ActorRef}
 import io.scalac.slack.bots.IncomingMessageListener
-import io.scalac.slack.common.{OutboundMessage, Ping}
+import io.scalac.slack.common.{DirectMessage, OutboundMessage, Ping}
 import io.scalac.slack.websockets.WebSocket
 
 class DigestBot extends IncomingMessageListener {
@@ -11,9 +11,28 @@ class DigestBot extends IncomingMessageListener {
 
   println(s"Starting $this")
 
+  var storage = List("link 1", "link 2")
+
+  def isStoreCommand(s: String) = {
+    println(s"IS $s store? ${s.startsWith("$digest-put")}")
+    s.startsWith("$digest-put")
+  }
+  def isShowAllCommand(s: String) = s.startsWith("$digest-get")
+
+  def put(linkToStore: String) = storage = linkToStore :: storage
+  def get() = storage
+
   def receive = {
-    case x =>
-      println(s"Got x= $x from Slack")
-      publish(OutboundMessage("C03DN1GUJ", "Digest!"))
+    case DirectMessage(txt) if isStoreCommand(txt) =>
+      println(s"Got x= $txt from Slack")
+      val link = txt.split(" ")(1)
+      put(link)
+      publish(OutboundMessage("C03DN1GUJ", s"Link $link stored for Digest!"))
+
+    case DirectMessage(txt) if isShowAllCommand(txt) =>
+      println(s"Got x= $txt from Slack")
+      publish(OutboundMessage("C03DN1GUJ", s"Digest contains: ${get().mkString(" ")}"))
+
+    case x => println(s"Digest ignores $x")
   }
 }
