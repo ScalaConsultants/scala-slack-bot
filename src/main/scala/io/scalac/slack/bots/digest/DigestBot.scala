e277c165fb226fa4b10b07f38a08da943eebcd8d
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-02-19 21:46:14 +0100

    persist digest links in file

--- src/main/scala/io/scalac/slack/bots/digest/DigestBot.scala
+++ src/main/scala/io/scalac/slack/bots/digest/DigestBot.scala
@@ -1,29 +1,38 @@
 package io.scalac.slack.bots.digest
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import java.io.FileWriter
+
 import io.scalac.slack.bots.IncomingMessageListener
-import io.scalac.slack.common.{Command, DirectMessage, OutboundMessage, Ping}
-import io.scalac.slack.websockets.WebSocket
+import io.scalac.slack.common.{Command, OutboundMessage}
+
+import scala.io.Source
+import scala.util.Try
 
 class DigestBot extends IncomingMessageListener {
 
-
   println(s"Starting $this")
 
-  var storage = List("link 1", "link 2")
+  val digestFile = "digest.txt"
 
-  def put(linkToStore: String) = storage = linkToStore :: storage
-  def get() = storage
+  def put(linkToStore: String) = {
+    val fw = new FileWriter(digestFile, true)
+    try {
+      fw.write(linkToStore)
+      fw.write("\n")
+    }
+    finally fw.close()
+  }
+  def get() = Try { Source.fromFile(digestFile).getLines() }
 
   def receive = {
-    case Command("digest-put", link :: _) =>
-      println(s"Got x= digest-put $link from Slack")
-      put(link)
-      publish(OutboundMessage("C03DN1GUJ", s"Link $link stored for Digest!"))
+    case Command("digest-put", links) =>
+      println(s"Got x= digest-put $links from Slack")
+      links.foreach(put(_))
+      publish(OutboundMessage("C03DN1GUJ", s"Link $links stored for Digest!"))
 
-    case Command("digest-get", _) =>
+    case Command("digest-list", _) =>
       println(s"Got x= digest-get from Slack")
-      publish(OutboundMessage("C03DN1GUJ", s"Digest contains: ${get().mkString(" ")}"))
+      get().map(d => publish(OutboundMessage("C03DN1GUJ", s"Digest contains: ${d.mkString(" ")}")) )
 
     case x => println(s"Digest ignores $x")
   }
