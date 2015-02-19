c3847a43f4d0a15e133c6d16262f7649220b526b
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-02-19 22:48:30 +0100

    reset command

--- src/main/scala/io/scalac/slack/bots/digest/DigestBot.scala
+++ src/main/scala/io/scalac/slack/bots/digest/DigestBot.scala
@@ -1,11 +1,9 @@
 package io.scalac.slack.bots.digest
 
-import java.io.FileWriter
+import java.io.{File, FileWriter}
 
-import io.scalac.slack.bots.IncomingMessageListener
 import io.scalac.slack.common.{Command, OutboundMessage}
 import io.scalac.slack.bots.IncomingMessageListener
-import io.scalac.slack.common._
 
 import scala.io.Source
 import scala.util.Try
@@ -19,22 +17,35 @@ class DigestBot extends IncomingMessageListener {
   def put(linkToStore: String) = {
     val fw = new FileWriter(digestFile, true)
     try {
-      fw.write(linkToStore)
-      fw.write("\n")
+      fw.append(linkToStore)
+      fw.append("\n")
     }
     finally fw.close()
   }
   def get() = Try { Source.fromFile(digestFile).getLines() }
+  def clear() = {
+    new File(digestFile).delete()
+    put("")
+  }
 
   def receive = {
-    case Command("digest-put", links, _) =>
+    case Command("digest-put", links, message) =>
       log.debug(s"Got x= digest-put $links from Slack")
       links.foreach(put(_))
-      publish(OutboundMessage("C03DN1GUJ", s"Link $links stored for Digest!"))
+      publish(OutboundMessage(message.channel, s"Link $links stored for Digest!"))
 
-    case Command("digest-list", _, _) =>
-      log.debug(s"Got x= digest-get from Slack")
-      get().map(d => publish(OutboundMessage("C03DN1GUJ", s"Digest contains: ${d.mkString(" ")}")) )
+    case Command("digest-list", _, message) =>
+      log.debug(s"Got x= digest-list from Slack")
+      publishContent(message.channel)
 
+    case Command("digest-clear", _, message) =>
+      log.debug(s"Got x= digest-clear from Slack")
+      publishContent(message.channel)
+      publish(OutboundMessage(message.channel, s"Now Clearing"))
+      clear()
+  }
+  
+  def publishContent(channel: String): Unit = {
+    get().map(d => publish(OutboundMessage(channel, s"Digest contained: ${d.mkString(" ")}")))
   }
 }
